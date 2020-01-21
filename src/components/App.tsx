5699aeef5c658eb1152eb4c73489dd0b4232e624
GeekEast <xiang.tan.p2109@gmail.com>
2020-01-21 14:28:43 +1030

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import SongDetail from './SongDetail';
 import SongList from './SongList';
 
+
 const App = (props: any) => {
   return (
     <div className="ui container grid">
       <div className="ui">
-        <div className="column eight wide">
+        <div>
           <SongDetail />
         </div>
-        <div className="column eight wide">
+        <div >
           <SongList />
         </div>
       </div>
